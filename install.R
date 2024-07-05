d3c033b7ca7a79ec493d3db38ec73f2936894ed9
Stevie Ped <stephen.pederson.au@gmail.com>
2024-07-05 11:33:19 +0930

    Added installation script

---
+++ install.R
@@ -0,0 +1,29 @@
+# Veyr basic installation script
+pkg <- c(
+  "AnnotationHub",
+  "Biostrings","BSgenome.Hsapiens.UCSC.hg38",
+  "cqn",
+  "DESeq2",
+  "edgeR", "ensembldb", "extraChIPs",
+  "GenomicFeatures", "Gviz", "glue", "ggrepel",
+  "IRanges",
+  "lme4", "lmerTest",
+  "pander", "plyranges",
+  "readxl", "rtracklayer",
+  "S4Vectors",
+  "tidyverse"
+)
+
+install.packages("BiocManager")
+stopifnot("BiocManager" %in% rownames(installed.packages()))
+cat("BiocManager successfully installed\n")
+BiocManager::install(pkg, ask = FALSE, force = TRUE)
+
+missing <- setdiff(pkg, rownames(installed.packages()))
+if (length(missing)) {
+  message("The following packages failed to install. Please email for assistance")
+  cat(missing, sep = "\n")
+  stop("Exiting installation")
+}
+
+message("All packages appear to be installed")
